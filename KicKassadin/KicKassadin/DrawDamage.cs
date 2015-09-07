2f1699085654153615178e0504635792d04a1249
theobjop <theobjop@live.com>
2015-09-07 17:53:42 -0500

    Kassadin Merge & Cleanup & Update
    Updated libraries and cleaned up code.

--- KicKassadin/KicKassadin/DrawDamage.cs
+++ KicKassadin/KicKassadin/DrawDamage.cs
@@ -1,10 +1,7 @@
 ﻿using LeagueSharp;
 using LeagueSharp.Common;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 
 namespace KicKassadin {
