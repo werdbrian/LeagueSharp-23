ca3e542d33ea6b2622c460a1a9c6e37e9fe8d090
werdbrian <werdbrian@gmail.com>
2015-09-15 15:32:19 -0700

    Update Drawings.cs

--- KicKassadin/KicKassadin/Drawings.cs
+++ KicKassadin/KicKassadin/Drawings.cs
@@ -23,8 +23,23 @@ namespace KicKassadin
             if (drawE)
                 if (KicKassadin.spells[Spells.E].Level > 0)
                     {
-                    //Render.Circle.DrawCircle(ObjectManager.Player.Position, KicKassadin.spells[Spells.E].Range, Color.White);
-                             
+                  //  Render.Circle.DrawCircle(ObjectManager.Player.Position, KicKassadin.spells[Spells.E].Range, Color.White);
+                    const int corkiERange = KicKassadin.spells[Spells.E].Range;
+                    Render.Circle.DrawCircle(ObjectManager.Player.Position, KicKassadin.spells[Spells.E].Range, KicKassadin.spells[Spells.E].IsReady() ? Color.Green : Color.Red);
+                    //var pos = ObjectManager.Player.Position.To2D() + 600 * ObjectManager.Player.Direction.To2D().Perpendicular();
+                    Drawing.DrawCircle(pos.To3D(), 50, KicKassadin.spells[Spells.E].IsReady() ? Color.Green : Color.Red);
+                    var playerPosition = ObjectManager.Player.Position.To2D();
+                    var direction = ObjectManager.Player.Direction.To2D().Perpendicular();
+                    var currentScreenPlayer = Drawing.WorldToScreen(ObjectManager.Player.Position);
+                    var currentAngel = 30 * (float) Math.PI / 180;
+                    var currentCheckPoint1 = playerPosition + corkiERange * direction.Rotated(currentAngel);
+                    currentAngel = 335 * (float) Math.PI / 180;
+                    var currentCheckPoint2 = playerPosition + corkiERange * direction.Rotated(currentAngel);
+                    var currentScreenCheckPoint1 = Drawing.WorldToScreen(currentCheckPoint1.To3D());
+                    var currentScreenCheckPoint2 = Drawing.WorldToScreen(currentCheckPoint2.To3D());
+                    Drawing.DrawLine(currentScreenPlayer.X, currentScreenPlayer.Y, currentScreenCheckPoint1.X, currentScreenCheckPoint1.Y, 2, Corki.spells[Spells.E].IsReady() ? Color.Green : Color.Red);
+                    Drawing.DrawLine(currentScreenPlayer.X, currentScreenPlayer.Y, currentScreenCheckPoint2.X, currentScreenCheckPoint2.Y,2,  Corki.spells[Spells.E].IsReady() ? Color.Green : Color.Red)
+                        
                     }
             if (drawW)
                 if (KicKassadin.spells[Spells.W].Level > 0)
