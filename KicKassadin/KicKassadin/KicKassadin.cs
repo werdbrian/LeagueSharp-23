d714e3f1fb9271f3a9a912983a3fde0feb4ec4c5
werdbrian <werdbrian@gmail.com>
2015-09-15 15:36:32 -0700

    Update KicKassadin.cs

--- KicKassadin/KicKassadin/KicKassadin.cs
+++ KicKassadin/KicKassadin/KicKassadin.cs
@@ -142,7 +142,6 @@ namespace KicKassadin {
 
             if (useR && spells[Spells.R].IsReady() && (!target.UnderTurret() || forceDive)) {
                 var extraEnemies = Player.Position.GetEnemiesInRange(1000).Count;
-                if (extraEnemies==0) return;
                 if (extraEnemies <= dontRCount)
                 {
                     spells[Spells.R].CastIfHitchanceEquals(target, CustomHitChance);
